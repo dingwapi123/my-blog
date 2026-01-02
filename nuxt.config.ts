69ab11f456af3a31d663ce51c27069ee80328159
cxz <1054757289@qq.com>
2026-01-02 22:56:10 +0800

    feat: 添加jsrepo配置和动画相关依赖
    添加jsrepo配置文件并集成motion-v和gsap等动画库
    更新nuxt配置和package.json依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/content', '@nuxt/image'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@nuxt/image',
+    'motion-v/nuxt',
+    '@vueuse/nuxt'
+  ],
 
   devtools: {
     enabled: true
